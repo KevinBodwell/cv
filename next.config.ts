849a89fc669e13da5ad8efb62444445a46a1d342
Kevin Bodwell <kevin.bodwell@gmail.com>
2025-06-30 13:36:18 -0400

    First new/standard deploy method

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  images: { unoptimized: true },
-  basePath: '/cv',
-  assetPrefix: '/cv/',  
+  images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
